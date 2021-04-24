953419835bfc1adf4e28860b9e82a4a56ca87858
mkryska11 <kryska.marek@gmail.com>
2021-04-24 19:21:02 +0200

    feat: debouncing and removal of parenthesis added, required packages updated

--- lyrics_finder.py
+++ lyrics_finder.py
@@ -1,9 +1,12 @@
+import re
 from lyrics_extractor import SongLyrics
+
 class LyricsFinder:
     __extract_lyrics = None
-    def __init__(self, api_key, engine_id):
-        self.__extract_lyrics = SongLyrics(api_key, engine_id)
+    def __init__(self):
+        self.__extract_lyrics = SongLyrics()
     def find_lyrics(self, title, artist):
+        title = re.sub('\(.*\)', '', title)
         result = self.__extract_lyrics.get_lyrics(f'{title} {artist}')
         lyrics = result.get('lyrics')
         return lyrics
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from windows_media_control.media_manager import MediaManager
 from gui import Gui
 from lyrics_finder import LyricsFinder
+from threading import Timer
+
 gui = Gui(15, 70)
-api_key = ''
-engine_id = ''
-lf = LyricsFinder(api_key, engine_id)
+lf = LyricsFinder()
+timer = None
+
+def set_timer(*args):
+    global timer
+    if timer and timer.is_alive():
+        timer.cancel()
+    timer = Timer(3.0, on_song_changed, args=args)
+    timer.start()
+    gui.set_text('searching...')
 
 def on_song_changed(session, args):
     if args.title and args.artist:
@@ -12,6 +21,6 @@ def on_song_changed(session, args):
         gui.set_text(lyrics)
 
 if __name__ == '__main__':
-    MediaManager.set_on_media_properties_changed(on_song_changed)
+    MediaManager.set_on_media_properties_changed(set_timer)
     MediaManager.start(send_mpc_after_csc=True)
     gui.run()
